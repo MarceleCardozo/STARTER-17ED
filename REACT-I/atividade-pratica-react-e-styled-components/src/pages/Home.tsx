9f933a7e35198bcf94e38f479bd9740cc7bec704
Marcele Cardozo <marcelecardozo@gmail.com>
2024-03-25 22:29:42 -0300

    aula 25.03.2024

---
+++ REACT-I/atividade-pratica-react-e-styled-components/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { Banner } from "../components/banner/Index";
+import { Cards } from "../components/cards";
+import AbcIcon from "@mui/icons-material/Abc";
+
+export const Home = () => {
+  return (
+    <>
+      <Banner />
+      <Cards paragraph="testes test" title="Titulo">
+        <AbcIcon />
+      </Cards>
+    </>
+  );
+};
